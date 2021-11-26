3afc797832ed5da761e7444f62381d5e4ab9894a
kumardinesh98 <devadineshkumar98@gmail.com>
2021-11-25 23:20:32 -0800

    using random to get die number 1 to 6

---
+++ SnakeAndLadderCase2.java
@@ -0,0 +1,8 @@
+public class SnakeAndLadderCase1 {
+	public static void main(String[] args) {
+		points = 0;
+		int die_number=(int)(Math.floor(Math.random() *10) % 6 )+1;
+		System.out.println(die_number);
+
+	}
+}
